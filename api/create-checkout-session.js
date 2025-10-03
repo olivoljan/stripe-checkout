6eb5fd56356b27617abd48f931368964657fc105
Cristi <cristi@Cristis-MacBook-Air.local>
2025-10-03 22:38:03 +0200

    Fix API handlers for Vercel

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -8,27 +8,19 @@ export default async function handler(req, res) {
   }
 
   try {
+    const { priceId } = req.body;
+
     const session = await stripe.checkout.sessions.create({
       mode: "subscription",
-      payment_method_types: ["card", "klarna"],
+      payment_method_types: ["card"],
       line_items: [
-        // 1 liter (1, 3, 6 months)
-        { price: "price_1SCnsoEKCocP3sE0nnVcw3B2", quantity: 1 },
-        { price: "price_1SCnsoEKCocP3sE08vL0YVc5", quantity: 1 },
-        { price: "price_1SCnsoEKCocP3sE0AEPypSLw", quantity: 1 },
-
-        // 2 liter (1, 3, 6 months)
-        { price: "price_1SCnrwEKCocP3sE0IEtNGNrf", quantity: 1 },
-        { price: "price_1SCnrwEKCocP3sE06kNcBy2X", quantity: 1 },
-        { price: "price_1SCnrwEKCocP3sE0QwtcBykt", quantity: 1 },
-
-        // 3 liter (1, 3, 6 months)
-        { price: "price_1SCneBEKCocP3sE035p9ANCI", quantity: 1 },
-        { price: "price_1SCneBEKCocP3sE0zItMU5kX", quantity: 1 },
-        { price: "price_1SCneBEKCocP3sE0JCfmIl3P", quantity: 1 },
+        {
+          price: priceId,
+          quantity: 1,
+        },
       ],
-      success_url: "https://olivolja-dev.webflow.io/tack",
-      cancel_url: "https://olivolja-dev.webflow.io/checkout",
+      success_url: "https://olivkassen.com/tack",
+      cancel_url: "https://olivkassen.com/avbruten",
     });
 
     res.status(200).json({ url: session.url });
